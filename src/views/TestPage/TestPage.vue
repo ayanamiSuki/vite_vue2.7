b7f1bcc5e2f0c9817948b3f9e8cc01090096a936
ayanamiSuki <61078613+ayanamiSuki@users.noreply.github.com>
2023-10-10 10:45:56 +0800

    Initial commit

---
+++ src/views/TestPage/TestPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test-page">
+    <div>route query: {{ queryData }}</div>
+    <the-chart></the-chart>
+  </div>
+</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router/composables'
+import TheChart from '/@/components/TheChart.vue'
+const $route = useRoute()
+const queryData = ref('')
+onMounted(() => {
+  const query = <string>$route.query.data
+  if (query) {
+    queryData.value = query
+  }
+})
+</script>
